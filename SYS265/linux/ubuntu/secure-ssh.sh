9c95577e41cb0f23765f61c2bbd29353c6ed950f
Cole Davis-Brand <cole@web01-cole.cole.local>
2022-02-20 15:01:06 -0500

    added script

---
+++ SYS265/linux/ubuntu/secure-ssh.sh
@@ -0,0 +1,12 @@
+#secure-ssh.sh
+#author cole
+#creates a new ssh user using $1 parameter
+sudo useradd -m -d /home/$1 -s /bin/bash/ $1
+sudo mkdir /home/$1/.ssh
+#adds a public key from the local repo
+sudo cp /home/cole/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
+#removes roots ability to ssh in for CENTOS machines
+sudo sed -i "s/#PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
