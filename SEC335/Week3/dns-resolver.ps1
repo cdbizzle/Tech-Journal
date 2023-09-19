a6fc35e203b6243544576842b889960a73d8d7c7
cdbizzle <71033480+cdbizzle@users.noreply.github.com>
2023-09-18 23:06:49 -0400

    Add files via upload

---
+++ SEC335/Week3/dns-resolver.ps1
@@ -0,0 +1,12 @@
+﻿param($prefix, $server)
+
+for ($i = 1; $i -le 255; $i++) {
+
+    $ip = "$prefix.$i"
+    $output = Resolve-DnsName -DnsOnly "$ip" -Server $server -ErrorAction Ignore
+    
+    if ($output) {
+        $nameHost = $output.NameHost
+        Write-Host "$ip - $nameHost"
+    }
+}
