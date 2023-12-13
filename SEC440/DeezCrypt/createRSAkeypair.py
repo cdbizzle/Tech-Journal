fffcb41888e9e2c87d7f3d0bd86de62042ab3dde
cdbizzle <71033480+cdbizzle@users.noreply.github.com>
2023-12-13 16:48:31 -0500

    Add files via upload

---
+++ SEC440/DeezCrypt/createRSAkeypair.py
@@ -0,0 +1,25 @@
+
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# Generate RSA keypair
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+
+# Save private key to a file
+with open("private_key.pem", "wb") as f:
+    f.write(private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    ))
+
+# Save public key to a file
+public_key = private_key.public_key()
+with open("public_key.pem", "wb") as f:
+    f.write(public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    ))
